42fa68ba33dba4872cd430af880f0ffbedf1a646
Gvozdov Maxim <mgvozdov@gmail.com>
2021-12-04 17:51:12 +0700

    Task #1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,8 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message = productName + ' за ' + productPrice + ' теперь в корзине!';
+    console.log(message);
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
@@ -23,7 +25,7 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
+    let newValue = oldValue + 1
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
@@ -36,6 +38,8 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum = (oldSum + difference) 
+    let newSumtext = newSum + ' ₽'
 
     // Задание №1.3. Увеличение суммы в корзине
 
