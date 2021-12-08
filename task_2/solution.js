f9ae77b6f5c1f838492ad6ba234c55c86aec50b2
Gvozdov Maxim <mgvozdov@gmail.com>
2021-12-09 01:02:08 +0700

    Task #2 Done v1.1

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,25 +2,21 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
-    let shippingSum;
+    // Задание №2.1. Рассчитать доставку
+    let shippingSum;  // создайте переменную shippingSum
 
     if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;}
-    
-    else 
-    {shippingSum = shippingPrice}
-    // Задание №2.1. Рассчитать доставку
-
-    // создайте переменную shippingSum
-
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
 
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-
+    else 
+    {shippingSum = shippingPrice}
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
+ 
 
     // Конец решения задания №2.1.
 
@@ -31,9 +27,7 @@ function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная цена для скидки
     let discountPart = discount; // величина скидки в процентах
-   
     
-
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
